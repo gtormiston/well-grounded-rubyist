175eb10019e2d6aa45cfde02d0c2f4e0966841f9
Graham Ormiston <grahamormiston@gmail.com>
2016-05-22 21:13:17 +0100

    First commit - chapter 9 hashes

---
+++ 9-3-1-creating-hashes.rb
@@ -0,0 +1,19 @@
+# literal
+hash = {} # empty hash
+
+# Hash.new constructor
+Hash.new
+
+# Hash.[] class method
+Hash["one", 1, "two", 2]
+
+{"one"=> 1, "two" => 2}
+
+Hash[[[1,2],[3,4],[5,6]]]
+
+{1=>2, 3=>4, 5=>6}
+
+
+
+
+
---
+++ 9-3-2-inserting-retrieving-removing-hash-pairs.rb
@@ -0,0 +1,14 @@
+# adding 
+
+state_hash["New York"] = "NY" # adds a key value pair to the hash state_hash
+# same as
+state_hash.[]=("New York", "NY")
+# same as store method
+state_hash.store("New York", "NY")
+
+# keys must be unique (values don't need to be) - if the same key, value is overwritten e.g.
+
+
+
+
+
